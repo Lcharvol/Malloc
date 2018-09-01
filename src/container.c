e1783c0c8a2ed2c016caa6e9d5427d2702d48f39
Lcharvol <charvolin0@gmail.com>
2018-09-01 17:47:28 +0200

    Norm container.c

--- src/container.c
+++ src/container.c
@@ -1,29 +1,44 @@
-# include "../includes/malloc.h"
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   container.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lcharvol <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/09/01 17:43:03 by lcharvol          #+#    #+#             */
+/*   Updated: 2018/09/01 17:47:04 by lcharvol         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-t_container   *create_container(size_t length, char *name)
+#include "../includes/malloc.h"
+
+t_container		*create_container(size_t length, char *name)
 {
-    t_container  *newContainer;
-    int     i;
-    i = 0;
-    
-    length = ((length + sizeof(t_container) - 1) /
-        getpagesize()) * getpagesize() + getpagesize();
-    if((newContainer = mmap(0, length, PROT_READ | PROT_WRITE, MAP_ANON | MAP_PRIVATE, -1, 0)) == MAP_FAILED)
-        return NULL;
-    newContainer->name = name;
-    newContainer->length = length - sizeof(t_container);
-    newContainer->next = NULL;
-    return newContainer;
+	t_container	*new_container;
+	int			i;
+
+	i = 0;
+	length = ((length + sizeof(t_container) - 1) /
+			getpagesize()) * getpagesize() + getpagesize();
+	if ((new_container = mmap(0, length, PROT_READ | PROT_WRITE, MAP_ANON
+					| MAP_PRIVATE, -1, 0)) == MAP_FAILED)
+		return (NULL);
+	new_container->name = name;
+	new_container->length = length - sizeof(t_container);
+	new_container->next = NULL;
+	return (new_container);
 }
 
-t_large  *create_large(size_t length)
+t_large			*create_large(size_t length)
 {
-    t_large  *newLarge;
+	t_large		*new_large;
 
-    length = ((length + sizeof(t_large) - 1) / getpagesize()) * getpagesize() + getpagesize();
-    if((newLarge = mmap(0, length, PROT_READ | PROT_WRITE, MAP_ANON | MAP_PRIVATE, -1, 0)) == MAP_FAILED)
-        return NULL;
-    newLarge->length = length - sizeof(t_large);
-    newLarge->next = NULL;
-    return newLarge;
+	length = ((length + sizeof(t_large) - 1) / getpagesize())
+		* getpagesize() + getpagesize();
+	if ((new_large = mmap(0, length, PROT_READ | PROT_WRITE, MAP_ANON
+					| MAP_PRIVATE, -1, 0)) == MAP_FAILED)
+		return (NULL);
+	new_large->length = length - sizeof(t_large);
+	new_large->next = NULL;
+	return (new_large);
 }
